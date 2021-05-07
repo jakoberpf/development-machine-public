3f6a3d90dd7dbc0b091167356e6bf1a697b05748
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-08 01:12:09 +0200

    update sync script

--- gitsync.sh
+++ gitsync.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+mkdir tmp
+git clone git@github.com:jakoberpf/development-machine.git tmp
+rm -rf tmp/.git
+mv tmp/* .
+rm -rf tmp
+
 # https://regex101.com/
-ls | grep -P "^.*\.(enc|gpg)$" | xargs -d"\n" rm
+ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep -E "^.*\.(enc|gpg|vault_pass)$" | xargs -d"\n" rm
+ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep 'vault.yaml' | xargs -d"\n" rm
 # How it works:
 # - ls lists all files (one by line since the result is piped).
 # - grep -P "^.*\.(enc|gpg)$" filters the list of files and leaves only those that match the regular expression ^.*\.(enc|gpg)$
