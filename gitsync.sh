0241096f4e0c410258aeeca39ba2a6a33365818f
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-05-14 17:42:00 +0200

    update readme and gitsync script

--- gitsync.sh
+++ gitsync.sh
@@ -5,12 +5,14 @@ cd "$parent_path"
 mkdir tmp
 git clone git@github.com:jakoberpf/development-machine.git tmp
 rm -rf tmp/.git
+rm -rf ansible
+rm -rf terraform
 mv tmp/* .
 rm -rf tmp
 
 # https://regex101.com/
-ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep -E "^.*\.(enc|gpg|vault_pass)$" | xargs -d"\n" rm
-ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep 'vault.yaml' | xargs -d"\n" rm
+ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep -E "^.*\.(enc|gpg|vault_pass)$" | xargs rm
+ls -RA | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }' | grep 'vault.yaml' | xargs rm
 # How it works:
 # - ls lists all files (one by line since the result is piped).
 # - grep -P "^.*\.(enc|gpg)$" filters the list of files and leaves only those that match the regular expression ^.*\.(enc|gpg)$
